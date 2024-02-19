e6ccf89247449c4439204cf7dba9230cb720e462
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-02-19 13:46:00 +0300

    Add new functions in library

---
+++ libs/data_structures/matrix/matrix.c
@@ -0,0 +1,63 @@
+#include "matrix.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <malloc.h>
+
+matrix getMemMatrix(int nRows, int nCols) {
+    int **values = (int **) malloc(sizeof(int *) * nRows);
+    for (int i = 0; i < nRows; i++)
+        values[i] = (int *) malloc(sizeof(int) * nCols);
+    return (matrix) {values, nRows, nCols};
+}
+
+matrix *getMemArrayOfMatrices(int nMatrices,
+                              int nRows, int nCols) {
+    matrix *ms = (matrix *) malloc(sizeof(matrix) * nMatrices);
+    for (int i = 0; i < nMatrices; i++)
+        ms[i] = getMemMatrix(nRows, nCols);
+    return ms;
+}
+
+void freeMemMatrix(matrix *m) {
+    for (int i = 0; i < m->nRows; i++) {
+        free(m->nCols[i]);
+    }
+    free(m->nCols);
+    free(m);
+}
+
+void freeMemMatrices(matrix *ms, int nMatrices) {
+    for (int i = 0; i < nMatrices; i++) {
+        freeMemMatrix(*ms[i]);
+    }
+}
+
+void inputMatrix(matrix *m) {
+    for (int i = 0; i < m->nRows; i++) {
+        for (int j = 0; j < m->nCols; j++) {
+            scanf("%d", &m->nCols[i][j]);
+        }
+    }
+}
+
+void inputMatrices(matrix *ms, int nMatrices) {
+    for (int i = 0; i < nMatrices; i++) {
+        inputMatrix(&ms[i]);
+    }
+}
+
+void outputMatrix(matrix m) {
+    for (int i = 0; i < m.nRows; i++) {
+        for (int j = 0; j < m.nCols[i]; j++) {
+            printf("%d ", m.nCols[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+void outputMatrices(matrix *ms, int nMatrices) {
+    for (int i = 0; i < nMatrices; i++) {
+        outputMatrix(ms[i]);
+    }
+}
---
+++ libs/data_structures/matrix/matrix.h
@@ -0,0 +1,31 @@
+#ifndef LABORATORY_15_MATRIX_H
+#define LABORATORY_15_MATRIX_H
+
+typedef struct matrix {
+    int **values; // элементы матрицы
+    int nRows; // количество рядов
+    int nCols; // количество столбцов
+} matrix;
+
+typedef struct position {
+    int rowIndex;
+    int colIndex;
+} position;
+
+matrix getMemMatrix(int nRows, int nCols);
+
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols)
+
+void freeMemMatrix(matrix *m);
+
+void freeMemMatrices(matrix *ms, int nMatrices);
+
+void inputMatrix(matrix *m);
+
+void inputMatrices(matrix *ms, int nMatrices);
+
+void outputMatrix(matrix m);
+
+void outputMatrices(matrix *ms, int nMatrices);
+
+#endif //LABORATORY_15_MATRIX_H
